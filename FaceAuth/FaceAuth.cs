8db7ae999d817d381119d64665d3b0a2a0c429c7
Or1onn <salaxetdinovorxan@gmail.com>
2023-06-22 01:24:24 +0400

    Initial commit

---
+++ FaceAuth/FaceAuth.cs
@@ -0,0 +1,41 @@
+﻿using Emgu.CV;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace FaceAuth;
+
+public class FaceAuth
+{
+    #region Vaiable
+    
+    private float FaceDistance = -1;
+    private  int FaceThreshold = 3000;
+    private string dbFolderName = "face_db";
+    private FaceRecognizer recognizer = new FisherFaceRecognizer(0, 3500); //4000
+    private List<Image<Gray, byte>> imgList = new();
+    private List<int> imgIds = new(){};
+    
+    #endregion
+
+    public void LoadFaceRecognizer()
+    {
+        if (!Directory.Exists(dbFolderName))
+        {
+            Directory.CreateDirectory(dbFolderName);
+            Directory.CreateDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\" + dbFolderName);
+        }
+        else
+        {
+            string[] files = Directory.GetFiles(dbFolderName, "*.jpg", SearchOption.AllDirectories);
+
+            Parallel.ForEach(files, file => { imgList.Add(new Image<Gray, byte>(file)); });
+
+            using VectorOfMat vectorOfMat = new VectorOfMat();
+            using VectorOfInt vectorOfInt = new VectorOfInt(imgIds.ToArray());
+            vectorOfMat.Push(imgList.ToArray());
+
+            recognizer.Train(vectorOfMat, vectorOfInt);
+        }
+    }
+}
