3cbdf5d28966fd88e26686951b89506bd665f115
Or1onn <salaxetdinovorxan@gmail.com>
2023-06-22 02:00:25 +0400

    RegisterIFace method added

--- FaceAuth/FaceAuth.cs
+++ FaceAuth/FaceAuth.cs
@@ -1,22 +1,38 @@
-﻿using Emgu.CV;
+﻿using System.Xml;
+using Emgu.CV;
 using Emgu.CV.Face;
 using Emgu.CV.Structure;
 using Emgu.CV.Util;
 
 namespace FaceAuth;
 
-public class FaceAuth
+public class FaceAuthProvider
 {
     #region Vaiable
-    
+
     private float FaceDistance = -1;
-    private  int FaceThreshold = 3000;
+    private int FaceThreshold = 3000;
     private string dbFolderName = "face_db";
     private FaceRecognizer recognizer = new FisherFaceRecognizer(0, 3500); //4000
     private List<Image<Gray, byte>> imgList = new();
-    private List<int> imgIds = new(){};
-    
+    private List<int> imgIds = new() { };
+    private bool isTrained = false;
+
     #endregion
+
+    public void RegisterIFace(string name, OpenCvSharp.Mat face)
+    {
+        Random rand = new Random();
+        string facename = "face_" + name + "_" + rand.Next() + ".jpg";
+        string folder = "TrainedFaces";
+        if (!Directory.Exists(folder))
+        {
+            Directory.CreateDirectory(folder);
+        }
+        facename = Path.Combine(folder, facename);
+
+        face.SaveImage(facename);
+    }
 
     public void LoadFaceRecognizer()
     {
@@ -29,13 +45,25 @@ public class FaceAuth
         {
             string[] files = Directory.GetFiles(dbFolderName, "*.jpg", SearchOption.AllDirectories);
 
-            Parallel.ForEach(files, file => { imgList.Add(new Image<Gray, byte>(file)); });
+            if (files.Length != 0)
+            {
+                Parallel.ForEach(files, file => { imgList.Add(new Image<Gray, byte>(file)); });
 
-            using VectorOfMat vectorOfMat = new VectorOfMat();
-            using VectorOfInt vectorOfInt = new VectorOfInt(imgIds.ToArray());
-            vectorOfMat.Push(imgList.ToArray());
+                using VectorOfMat vectorOfMat = new VectorOfMat();
+                using VectorOfInt vectorOfInt = new VectorOfInt(imgIds.ToArray());
+                vectorOfMat.Push(imgList.ToArray());
 
-            recognizer.Train(vectorOfMat, vectorOfInt);
+                try
+                {
+                    recognizer.Train(vectorOfMat, vectorOfInt);
+                    isTrained = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+            }
         }
     }
 }
